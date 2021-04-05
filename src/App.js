cb2606499c6ec7cd1ad40770f8d1908a5fe3b075
Eka Garibashvili <you@example.com>
2021-04-05 15:40:44 +0400

    quotes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [quotes, setQuote] = useState('');
+
+  const newQuote = () =>{
+    fetch('https://type.fit/api/quotes')
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuote(data[randomNum]);
+    });
+  };
+
+  useEffect(() => {
+    newQuote();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote-box">
+        
+        <p className="quote">"{quotes.text}"</p>
+        <p className="author">--{quotes.author}</p>
+        <div className="btns">
+         <button className="click" onClick={newQuote}>New Quote</button>
+         <a className="tweet" href="twitter.com/intent/tweet" target="_blank">Tweet</a>
+        </div>
+      </div>
     </div>
   );
 }
